59bf454cc08a3aed86e81cafc8a4b769f3853756
dobstotev <35801206+dobstotev@users.noreply.github.com>
2019-12-07 21:45:56 +0100

    Creating the base for the custom white button class

--- CoTraveller-App/CoTraveller-App/View/CoTravellerCustomView.swift
+++ CoTraveller-App/CoTraveller-App/View/CoTravellerCustomView.swift
@@ -15,3 +15,24 @@ class infoBoxNeonGreen: UIView {
         backgroundColor = #colorLiteral(red: 0.2235294118, green: 0.9333333333, blue: 0.5960784314, alpha: 1)
     }
 }
+
+class whiteCTA: UIButton {
+
+    //Initializig the new custom button style
+    override init (frame: CGRect) {
+        super.init(frame: frame)
+        setUpButton()
+    }
+    
+    //Initializig the new custom button style for storyboard
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpButton()
+    }
+    
+    func setUpButton() {
+        
+    }
+    
+    
+}
