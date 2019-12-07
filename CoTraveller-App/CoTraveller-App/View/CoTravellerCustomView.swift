8a5dcbf8c67c36f4b78a2f918587e4f3a34e4e01
dobstotev <35801206+dobstotev@users.noreply.github.com>
2019-12-07 20:12:36 +0100

    Creating the view file and the properties for the Neon Box

---
+++ CoTraveller-App/CoTraveller-App/View/CoTravellerCustomView.swift
@@ -0,0 +1,20 @@
+//
+//  CoTravellerCustomView.swift
+//  CoTraveller-App
+//
+//  Created by Dobs Totev on 07/12/2019.
+//  Copyright © 2019 d.DOBS Creative Ltd. All rights reserved.
+//
+
+import UIKit
+
+class drawBox {
+    
+}
+
+class infoBoxNeonGreen: UIView {
+    override func awakeFromNib() {
+        layer.cornerRadius = 10
+        backgroundColor = #colorLiteral(red: 0.2235294118, green: 0.9333333333, blue: 0.5960784314, alpha: 1)
+    }
+}
